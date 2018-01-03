2ecee78f53c9c68a323e28ef85c51db1b2cb2aed
Zac Duvall <duvallzac@gmail.com>
2018-01-03 01:38:06 -0700

    Update typing animation to much leaner code.

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,58 +1,58 @@
-var TxtRotate = function (el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = "";
-    this.tick();
-    this.isDeleting = false;
-};
+// var SPY = (function () {
+//     function e(a, d, b) {
+//         var c, f, g, h;
+//         b == a.length
+//             ? (k.animationComplete = !0)
+//             : ((g = d.innerHTML),
+//                 (h = Math.floor(21 * Math.random() + 5)),
+//                 (c = 32 === a[b] ? 32 : a[b] - h),
+//                 (f = setInterval(function () {
+//                     d.innerHTML = g + String.fromCharCode(c);
+//                     c == a[b]
+//                         ? (clearInterval(f),
+//                             (c = 32),
+//                             b++ ,
+//                             setTimeout(function () {
+//                                 e(a, d, b);
+//                             }, 10))
+//                         : c++;
+//                 }, 5)));
+//     }
+//     var k = {};
+//     return (k = {
+//         animationComplete: !1,
+//         text: function (a) {
+//             this.animationComplete = !1;
+//             a = document.getElementById(a);
+//             for (var d = a.innerHTML, b = [], c = 0; c < d.length; c++) {
+//                 b.push(d.charCodeAt(c));
+//             }
+//             a.innerHTML = "";
+//             e(b, a, 0);
+//         }
+//     });
+// })();
 
-TxtRotate.prototype.tick = function () {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
+// $("document").ready(function () {
+//     // Hide header
+//     $(".hackView").hide();
+//     // Transition background
+//     $(".london").fadeIn(1200);
 
-    if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
+//     // Timeout for crypto text
+//     setTimeout(function () {
+//         $(".hackView").fadeIn("slow");
+//         SPY.text("op");
+//     }, 1000);
+// });
 
-    this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
 
-    var that = this;
-    var delta = 300 - Math.random() * 150;
-
-    if (this.isDeleting) {
-        delta /= 2;
-    }
-
-    if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === "") {
-        this.isDeleting = false;
-        this.loopNum++;
-        delta = 500;
-    }
-
-    setTimeout(function () {
-        that.tick();
-    }, delta);
-};
-
-window.onload = function () {
-    var elements = document.getElementsByClassName("txt-rotate");
-    for (var i = 0; i < elements.length; i++) {
-        var toRotate = elements[i].getAttribute("data-rotate");
-        var period = elements[i].getAttribute("data-period");
-        if (toRotate) {
-            new TxtRotate(elements[i], JSON.parse(toRotate), period);
-        }
-    }
-    // INJECT CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
-    document.body.appendChild(css);
-};
+new TypeIt('#example1', {
+    strings: ["Hello! I'm Zac", "I also like to write and make music."],
+    speed: 150,
+    breakLines: false,
+    loop: true,
+    loopDelay: 1000,
+    autoStart: true,
+    lifeLike: true
+});
